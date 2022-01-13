a3b49a3c2a59782314e15a99463a7810715b4c76
Bhargavi-ng <bhargavi.ng@gmail.com>
2022-01-13 00:51:01 -0500

    Imported the data and created a linear regression model and generated summary.

---
+++ R_Analysis/MechaCarChallenge.R
@@ -0,0 +1,15 @@
+# Deliverable 1
+
+# Import library
+library(dplyr)
+
+# Read file
+MechaCar <- read.csv(file='MechaCar_mpg.csv', check.names= F, stringsAsFactors= F)
+
+# Perform linear regression
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar)
+
+#Generate summary statistics for p-value and r-squared value
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar))
+
+
